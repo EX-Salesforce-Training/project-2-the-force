8624bd1ed557ebfcfbd35d9929c27fbe8da5b161
The Farrier <nkfarrey@gmail.com>
2021-05-14 14:37:09 -0500

    Question Answers dynamically generated & scrambled

--- theLanguageEmporium/force-app/main/default/aura/QuizQuestion/QuizQuestionController.js
+++ theLanguageEmporium/force-app/main/default/aura/QuizQuestion/QuizQuestionController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
+    initQuestion : function(component, event, helper) {
+        helper.scrambleAnswers(component);
     }
 })
--- theLanguageEmporium/force-app/main/default/aura/QuizQuestion/QuizQuestionHelper.js
+++ theLanguageEmporium/force-app/main/default/aura/QuizQuestion/QuizQuestionHelper.js
@@ -1,5 +1,38 @@
 ({
-    helperMethod : function() {
+    scrambleAnswers : function(component) {
+        let questionObj = component.get("{!v.fields}");
+        let answerArray = [
+            {number: 1, correct: true, text: questionObj.Correct_Answer__c},
+            {number: 2, correct: false, text: questionObj.Option_2__c},
+            {number: 3, correct: false, text: questionObj.Option_3__c},
+            {number: 4, correct: false, text: questionObj.Option_4__c},
+        ];
+        console.log("Time to scramble answers");
 
+        shuffle(answerArray);
+
+        function shuffle(answerArray) {
+            var currentIndex = answerArray.length, temporaryValue, randomIndex;
+          
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+          
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex -= 1;
+          
+              // And swap it with the current element.
+              temporaryValue = answerArray[currentIndex];
+              answerArray[currentIndex] = answerArray[randomIndex];
+              answerArray[randomIndex] = temporaryValue;
+            }
+          
+            return answerArray;
+          };
+
+        component.set("{!v.answerList}",answerArray)
+        
+        console.log(questionObj.Prompt__c);
+        console.log(answerArray);
     }
 })
