231b0e62ef98ca6c97713e0b481bd3109cf2efe0
The Farrier <nkfarrey@gmail.com>
2021-05-14 10:46:26 -0500

    created init action to get quizzes/lessons

--- theLanguageEmporium/force-app/main/default/aura/CoursePageComponent/CoursePageComponentController.js
+++ theLanguageEmporium/force-app/main/default/aura/CoursePageComponent/CoursePageComponentController.js
@@ -1,13 +1,46 @@
 ({
     initPage : function(component, event, helper){
+        console.log("init controller")
         helper.retrieveCourseContent(component);
     },
-    showDisplay : function(component, event, helper) {
-        var displayDiv = component.find("course-display");
-        $A.util.addClass(displayDiv,"toggle-hide");
+    startQuiz : function(component, event, helper){
+        //Hide the basic display
+        helper.hideDisplay(component);
+        helper.setQuiz(component);
+
+        //change page layout to Quiz mode
+        component.set("v.activeQuiz", "true");
+        console.log("Quiz Started");
+
     },
-    hideDisplay : function(component, event, helper) {
-        var displayDiv = component.find("course-display");
-        $A.util.removeClass(displayDiv,"toggle-hide");
+    endQuiz : function(component, event, helper){
+        //Hide the basic display
+        helper.showDisplay(component);
+        component.set("v.activeQuiz", "false");
+        console.log("Quiz Ended");
+        
+        //Get the clicked quiz id and set it
+        //set active quiz to true
+
+    },
+    startLesson : function(component, event, helper){
+        //Hide the basic display
+        helper.hideDisplay(component);
+        component.set("v.activeLesson", "true")
+        console.log("Lesson Started");
+        
+        //Get the clicked quiz id and set it
+        //set active quiz to true
+
+    },
+    endLesson : function(component, event, helper){
+        //Hide the basic display
+        helper.showDisplay(component);
+        component.set("v.activeLesson", "false")
+        console.log("Lesson Ended");
+        
+        //Get the clicked quiz id and set it
+        //set active quiz to true
+
     },
 })
--- theLanguageEmporium/force-app/main/default/aura/CoursePageComponent/CoursePageComponentHelper.js
+++ theLanguageEmporium/force-app/main/default/aura/CoursePageComponent/CoursePageComponentHelper.js
@@ -1,15 +1,52 @@
 ({
     retrieveCourseContent : function(component) {
-        let apexMethod = component.get("c.CourseContent");
+        console.log("init Helper")
+        let lessonMethod = component.get("c.getLessons");
         //send course ID to APEX controller to query for related content
-        apexMethod.setParams({courseId: component.get("v.courseId")})
+        lessonMethod.setParams({courseId: component.get("v.courseId")})
         
-        apexMethod.setCallback(this, function(response){
+        lessonMethod.setCallback(this, function(response){
             if(response.getState() == "SUCCESS"){
+                component.set("v.lessonList", response.getReturnValue());
+                console.log("Returned Lessons")
+                console.log(response.getReturnValue())
                 //get Course name, set v.courseName
                 //get Lesson names and descriptions, use for iterator
                 //get Quiz names and grades, use for iterator
             }
         })
-    }
+
+        let quizMethod = component.get("c.getQuizzes");
+        //send course ID to APEX controller to query for related content
+        quizMethod.setParams({courseId: component.get("v.courseId")})
+        
+        quizMethod.setCallback(this, function(response){
+            if(response.getState() == "SUCCESS"){
+                component.set("v.quizList", response.getReturnValue());
+                console.log("Returned Quizzes")
+                console.log(response.getReturnValue())
+                console.log("Set Quizzes")
+                console.log(component.get("v.quizList"))
+                //get Course name, set v.courseName
+                //get Lesson names and descriptions, use for iterator
+                //get Quiz names and grades, use for iterator
+            }
+        })
+        $A.enqueueAction(lessonMethod);
+        $A.enqueueAction(quizMethod);
+    },
+    setQuiz : function(component, event, helper) {
+        
+    },
+    setLesson : function(component, event, helper) {
+        
+    },
+    hideDisplay : function(component, event, helper) {
+        var displayDiv = component.find("course-display");
+        $A.util.addClass(displayDiv,"toggle-hide");
+    },
+    showDisplay : function(component, event, helper) {
+        var displayDiv = component.find("course-display");
+        $A.util.removeClass(displayDiv,"toggle-hide");
+    },
 })
