485819580ca193ad41afde19aa0765114b96b451
The Farrier <nkfarrey@gmail.com>
2021-05-17 18:39:32 -0500

    added course name to event attributes

--- theLanguageEmporium/force-app/main/default/aura/CourseInAvailableCourses/CourseInAvailableCoursesHelper.js
+++ theLanguageEmporium/force-app/main/default/aura/CourseInAvailableCourses/CourseInAvailableCoursesHelper.js
@@ -1,8 +1,10 @@
 ({
     addToCart : function(component,event) {
         var thisCourseId = component.get("{!v.courseID}");
+        var thisCourseName = component.get("{!v.courseName}");
         event.setParams({
-            "courseId": thisCourseId
+            "courseId": thisCourseId,
+            "courseName": thisCourseName
         })
         console.log("You tried to add this to the cart")
         console.log(component.get("{!v.courseID}"))
