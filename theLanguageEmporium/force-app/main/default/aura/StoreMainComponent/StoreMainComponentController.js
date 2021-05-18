629c85df6189cd5a35c5f982b1fb56e23cb8544a
The Farrier <nkfarrey@gmail.com>
2021-05-17 19:09:25 -0500

    store cmp handles evt, passes items to cart cmp

--- theLanguageEmporium/force-app/main/default/aura/StoreMainComponent/StoreMainComponentController.js
+++ theLanguageEmporium/force-app/main/default/aura/StoreMainComponent/StoreMainComponentController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
+    updateCart : function(component, event, helper) {
+        helper.addNewItem(component,event);
     }
 })
--- theLanguageEmporium/force-app/main/default/aura/StoreMainComponent/StoreMainComponentHelper.js
+++ theLanguageEmporium/force-app/main/default/aura/StoreMainComponent/StoreMainComponentHelper.js
@@ -1,5 +1,10 @@
 ({
-    helperMethod : function() {
+    addNewItem : function(component,event) {
+        var currentCartList = component.get("{!v.shoppingCartItems}");
 
+        var newItem = event.getParam("courseId");
+        currentCartList.push(newItem);
+
+        component.set("{!v.shoppingCartItems}", currentCartList);
     }
 })
