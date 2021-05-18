75f8e5a47f63fda62959b5ba34ece17b0f468d00
The Farrier <nkfarrey@gmail.com>
2021-05-17 22:00:24 -0500

    Aura if doesn't work, lol. Thanks salesforce

--- theLanguageEmporium/force-app/main/default/aura/NavbarComponent/NavbarComponentController.js
+++ theLanguageEmporium/force-app/main/default/aura/NavbarComponent/NavbarComponentController.js
@@ -1,5 +1,18 @@
 ({
-    myAction : function(component, event, helper) {
+    navHome : function(component, event, helper) {
+        helper.renderHomePage(component);
+
+    },
+    navAccount : function(component, event, helper) {
+        helper.renderAccountPage(component);
+
+    },
+    navShop : function(component, event, helper) {
+        helper.renderShopPage(component);
+
+    },
+    navCareers : function(component, event, helper) {
+        helper.renderCareersPage(component);
 
     }
 })
--- theLanguageEmporium/force-app/main/default/aura/NavbarComponent/NavbarComponentHelper.js
+++ theLanguageEmporium/force-app/main/default/aura/NavbarComponent/NavbarComponentHelper.js
@@ -1,5 +1,37 @@
 ({
-    helperMethod : function() {
-
+    renderHomePage : function(component, event, helper) {
+        var navEvent = component.getEvent("navEvent");
+        navEvent.setParams({
+            "page" : "Home",
+        });
+        navEvent.fire();
+    },
+    renderAccountPage : function(component, event, helper) {
+        var navEvent = component.getEvent("navEvent");
+        navEvent.setParams({
+            "page" : "LearnerPortal",
+        });
+        navEvent.fire();
+    },
+    renderShopPage : function(component, event, helper) {
+        var navEvent = component.getEvent("navEvent");
+        navEvent.setParams({
+            "page" : "Shop",
+        });
+        navEvent.fire();
+    },
+    renderCareersPage : function(component, event, helper) {
+        var navEvent = component.getEvent("navEvent");
+        navEvent.setParams({
+            "page" : "Careers",
+        });
+        navEvent.fire();
+    },
+    fireNavigator : function(component, event, helper){
+        var navEvent = component.getEvent("navEvent");
+        navEvent.setParams({
+            "page" : component.get(""),
+        });
+        navEvent.fire();
     }
 })
